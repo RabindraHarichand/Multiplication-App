af0a3fdabb7418cd6f0831a9068fac55641847da
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-11-20 13:38:15 -0400

    (refactoring): Refactorization of multiplication app to implement clean architecture. Implementation of save file use case & create table use case

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { yarg } from "./config/plugins/yargs.plugin";
+import { ServerApp } from "./presentation/server-app";
 
 // console.log(process.argv);
 // console.log(yarg.b)
@@ -7,5 +8,12 @@ import { yarg } from "./config/plugins/yargs.plugin";
 })();
 
 async function main() {
-  console.log(yarg);
+  const {
+    b: base,
+    l: limit,
+    s: showTable,
+    n: fileName,
+    d: fileDestination,
+  } = yarg;
+  ServerApp.run({ base, limit, showTable, fileName, fileDestination });
 }
